d89ac1b48e81074cb95313ba692f004259b5fdc2
maximevolders <maxime.volders@hotmail.be>
2020-11-17 17:05:04 +0100

    Tâche 2.2
    Le script bash pour les perf est encore à faire

--- test_and_set.c
+++ test_and_set.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
 volatile int verrou=0;
+int N;
+
+void error(int err, char *msg) {
+    fprintf(stderr,"%s a retourné %d message d'erreur : %s\n",msg,err,strerror(errno));
+    exit(EXIT_FAILURE);
+}
 
 void lock(){
 	int test=1;
@@ -18,11 +29,39 @@ void unlock(){
 		:"=&r"(verrou));
 }
 
-int main(int argc, char argv[]){
-	printf("Verrou avant lock: %d\n", verrou);
+void* test(){
+	for(int i=0; i<6400/N; i++){
+		lock();
+		while(rand() > RAND_MAX/1000);
+		unlock();
+	}
+	return (NULL);
+}
+
+int main(int argc, char *argv[]){
+/*	printf("Verrou avant lock: %d\n", verrou);
 	lock();
 	printf("Verrou après lock: %d\n", verrou);
 	unlock();
-	printf("Verrou après unlock: %d\n", verrou);
-	return 0;
+	printf("Verrou après unlock: %d\n", verrou); */
+	
+	int err;
+	
+	if(argc != 2) return (EXIT_FAILURE);
+	N = atoi(argv[1]); // Nombre de threads
+	
+	pthread_t thread[N];
+	
+    for (int i=0; i<N; i++) { // On crée les threads
+        err=pthread_create(&(thread[i]),NULL,test,NULL);
+        if(err!=0)
+            error(err,"pthread_create");
+    }
+	
+	for(int i=0; i<N; i++) { // On attend que les threads se terminent
+		err=pthread_join(thread[i],NULL);
+        if(err!=0)
+            error(err,"pthread_join");
+	}
+	return (EXIT_SUCCESS);
 }
