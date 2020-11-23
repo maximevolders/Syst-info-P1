a9529e61ccd8bdd4a2d8e4d7cea4efff8c515db1
maximevolders <maxime.volders@hotmail.be>
2020-11-23 09:49:45 +0100

    Commentaires python

--- graph.py
+++ graph.py
@@ -1,40 +1,58 @@
 #!/usr/bin/env python3
 
+''' 
+Fonction pour ploter les graphes de performance des différents problèmes.
+Il y a une fonction par problème afin de ploter les problèmes un par un.
+Sur un même graphe, on plot le problème avec les verrous POSIX et avec l'attente active.
+    
+Les commentaires sont uniquement dans la première fonction, mais les trois fonctions sont similaires.
+'''
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
 def philosophes():
+    #Import data POSIX
     data1 = pd.read_csv("mesures1posix.csv")
     
+    #Calcul de la moyenne par thread, l'écart type par thread et du nombre de threads POSIX
     mean1 = data1.groupby(["thread"]).mean()["time"]
     std1 = data1.groupby(["thread"]).std()["time"]
     threads1 = data1["thread"].unique()
     
+    #import data attente active
     data2 = pd.read_csv("mesures1AttAct.csv")
     
+    #Calcul de la moyenne par thread, l'écart type par thread et du nombre de threads attente active
     mean2 = data2.groupby(["thread"]).mean()["time"]
     std2 = data2.groupby(["thread"]).std()["time"]
     threads2 = data2["thread"].unique()
     
+    #Création du graph
     fig1 = plt.figure()
     
+    #Plot de la moyenne et de l'erreur concernant les verrous POSIX
     plt.plot(threads1, mean1, color="blue", linewidth=1.0, linestyle="-")
     plt.errorbar(threads1, mean1, yerr=std1, fmt='-o', color="blue")
     
+    #Plot de la moyenne et de l'erreur concernant les verrous par attente active
     plt.plot(threads2, mean2, color="red", linewidth=1.0, linestyle="-")
     plt.errorbar(threads2, mean2, yerr=std2, fmt='-o', color="red")
     
+    #Modification des limites des axes du graphiques ([0;max+1])
     plt.xlim(0,np.max(data1["thread"])+1)
     plt.ylim(0,np.max(data2["time"])+1)
     
+    #Légende des axes, titre du graphique, grille, légende du graphique
     plt.xlabel('# threads')
     plt.ylabel('Temps [s]')
     plt.title("Problème des philosophes")
     plt.grid(True)
     plt.legend(['POSIX','Attente active'], loc = 'upper left')
     
+    #Enregistrement de la figure
     plt.savefig("Philosophes.png")
     plt.savefig("Philosophes.pdf")
     
@@ -114,5 +132,5 @@ def read_write():
     plt.close()
     
 philosophes()
-#prod_cons()
-#read_write()
+prod_cons()
+read_write()
